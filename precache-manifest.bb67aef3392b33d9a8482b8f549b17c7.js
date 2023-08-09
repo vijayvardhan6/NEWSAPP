d614ab63d1140c3d7aeef14c154c4af8729e53dd
vijayvardhan6 <vijayvardhan672001@gmail.com>
2023-08-09 20:38:13 +0530

    Updates

---
+++ precache-manifest.bb67aef3392b33d9a8482b8f549b17c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cfbb7790aee8aa6bcbf",
+    "url": "/NEWSAPP/static/css/main.1c10285c.chunk.css"
+  },
+  {
+    "revision": "5cfbb7790aee8aa6bcbf",
+    "url": "/NEWSAPP/static/js/main.5cfbb779.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/NEWSAPP/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "65880eed52bf03343cda",
+    "url": "/NEWSAPP/static/js/2.65880eed.chunk.js"
+  },
+  {
+    "revision": "29aee456ce0d638e5feb",
+    "url": "/NEWSAPP/static/js/runtime~main.29aee456.js"
+  },
+  {
+    "revision": "c7cbd22f5787bb1aa6c249f1b3d2927e",
+    "url": "/NEWSAPP/index.html"
+  }
+];
